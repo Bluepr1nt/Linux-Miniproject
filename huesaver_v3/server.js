f0151b5f71a3453c1690ff3f1ae6117f0ffbe115
Jeroen <jeroenvanos2@gmail.com>
2016-04-05 16:14:27 +0200

    second commit

--- huesaver_v3/server.js
+++ huesaver_v3/server.js
@@ -2,12 +2,16 @@ var express = require('express');
 var settings = require('./settings.json');
 var parser = require('body-parser');
 var HueApi = require('node-hue-api').HueApi;
+var fs = require('fs');
+var gpio = require('gpio');
 
 var api = HueApi;
 var app = express();
 app.set('hueIP', settings.hueIP);
 app.set('deviceDescription', settings.deviceDescription);
 app.set('username', settings.username);
+
+var run = true;
 
 var displayResult = function(result){
 	console.log(JSON.stringify(result, null, 2));
@@ -20,16 +24,18 @@ var displayUserResult = function(result){
 	fs.writeFile('./settings.json', JSON.stringify(settings, null, 2), function (err) {
 		if (err) return console.log(err)
 	});
+	app.set('username', settings.username);
 }
 
 var displayError = function(err) {
     console.log(err);
 };
 
-var getNewUsername = function(result){
+var getNewUsername = function(){
 	console.log('user');
 	var hue = new HueApi();
 	hue.registerUser(app.get('hueIP'), app.get('deviceDescription'))
+	.then(console.log('name'))
 	.then(displayUserResult)
 	.fail(displayError)
 	.done();
@@ -40,13 +46,14 @@ var makeConnectionWithBride = function(result){
 	console.log(username);
 	if(username === "null" | username.lenght < 5 | username == ""){
 		getNewUsername();
-		makeConnectionWithBride();
 	}
 	
 	api = new HueApi(app.get('hueIP'), app.get('username'));
 	api.getConfig();
 }
-
-makeConnectionWithBride();
+if(run){
+	makeConnectionWithBride();
+	run = false;
+}
 
 //hue.imegumii.space 367cac47bb5b4bf26fb8e3787818af
